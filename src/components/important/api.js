63d406ed7985b23987164b3e710bd981a5f78a6e
deepak7691 <kawliya11@gmail.com>
2023-03-21 10:34:21 +0530

    deepak

---
+++ src/components/important/api.js
@@ -0,0 +1,9 @@
+export const apiUrl = {
+    
+    key : "83e51a92af33e7b2703ea7db03f8fa11",
+    base : "https://api.themoviedb.org/3/",
+    getGenres : "genre/movie/list",
+    nowPlaying : "movie/now_playing",
+    language : "language=en-US",
+    imageBase : "https://image.tmdb.org/t/p/w500"
+}
