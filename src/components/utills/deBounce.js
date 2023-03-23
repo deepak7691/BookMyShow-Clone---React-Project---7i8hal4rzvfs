5cd7cba1a5b852e924d0d608d465823febb2c387
deepak7691 <kawliya11@gmail.com>
2023-03-23 17:36:00 +0530

    deepak

--- src/components/utills/deBounce.js
+++ src/components/utills/deBounce.js
@@ -1,11 +1,11 @@
-function deBounce(callBack,delay=2000) {
-    let timer;
-    return () => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callBack();
-        },delay)
-    }
-}
+// function deBounce(callBack,delay=2000) {
+//     let timer;
+//     return () => {
+//         clearTimeout(timer);
+//         timer = setTimeout(() => {
+//             callBack();
+//         },delay)
+//     }
+// }
 
-export default deBounce;
+// export default deBounce;
